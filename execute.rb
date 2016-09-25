26c76a33c4b2eca03e3112c46abc43d4b5e6ee64
Luke Roberts <email@luke-roberts.co.uk>
2016-09-25 13:26:14 +0100

    use new format

--- execute.rb
+++ execute.rb
@@ -5,41 +5,37 @@ require "json"
 changed_files = ENV.fetch("CHANGED_FILES").split(' ')
 reportable_files = []
 
+Dir.chdir('/code')
+
 changed_files.each do |file|
   percentage_change = 0
   puts "#{file}"
   size_before = File.stat(file).size
-  
+
   if file.match(/.+\.png\z/i)
     system "pngquant --force --ext=.png #{file}"
     size_after = File.stat(file).size
     size_diff = size_before-size_after
     percentage_change = (1-(size_after.to_f/size_before))*100
-    
+
   elsif file.match(/.+\.jpe?g\z/i)
     system "mozjpeg -copy none -optimize -outfile #{file} #{file}"
     size_after = File.stat(file).size
     size_diff = size_before-size_after
     percentage_change = (1-(size_after.to_f/size_before))*100
   end
-  
+
   # check how much was saved, if its negligable then that's probably not a change worth making as it will only reduce quality with little benefit.
   if percentage_change < 1
     puts "#{file} optimization not enough to report"
     system "git checkout #{file}" 
   else
-    # record discovery
-    reportable_files << {
-      file: file,
-      file_name: file.split('/').last,
-      size_diff: size_diff,
-      percentage_change: percentage_change.round(2)
-    }
+    reportable_files << file
   end
 end
 
 branch = "pushbit/smusher"
-if changed_files.length > 0
+if reportable_files.length > 0
   branch += "-#{ENV.fetch('GITHUB_NUMBER')}"
 end
 
@@ -48,37 +44,37 @@ if reportable_files.length > 0
   system "git checkout -B #{branch}"
 
   puts "Adding updated files"
-  system "git add #{reportable_files.map{|f| f[:file] }.join(" ")}"
+  system "git add #{reportable_files.join(" ")}"
 
   puts "Commiting changed files"
   system "git commit -m \"Compressed image files\""
 
   puts "Pushing branch"
   system "git push -f origin #{branch}"
-  
+
   conn = Faraday.new(:url => ENV.fetch("APP_URL")) do |config|
     config.adapter Faraday.default_adapter
   end
-  
-  reportable_files.each do |file|
-    discovery = {
-      title: "#{file[:file_name]} compressed (#{file[:percentage_change]}% smaller)",
-      task_id: ENV.fetch("TASK_ID"),
-      kind: :optimization,
-      identifier: "smusher-#{file}",
-      branch: branch,
-      code_changed: true,
-      priority: :low
-    }
 
-    puts "Posting discovery"
-    res = conn.post do |req|
-      req.url '/discoveries'
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "Basic #{ENV.fetch("ACCESS_TOKEN")}"
-      req.body = discovery.to_json
-    end
+  payload = {
+    title: "#{reportable_files.length} files compressed.",
+    body: "Pushbit bot found #{reportable_files.length} files that were a little bigger then this needed to be",
+    task_id: ENV.fetch("TASK_ID"),
+    kind: "pull_request",
+    identifier: "smusher-#{ENV.fetch('GITHUB_NUMBER')}",
+    branch: branch,
+    base_branch: ENV.fetch("BASE_BRANCH"),
+    code_changed: true
+  }
+
+  puts "Posting pull_request actions"
+  res = conn.post do |req|
+    req.url '/actions'
+    req.headers['Content-Type'] = 'application/json'
+    req.headers['Authorization'] = "Basic #{ENV.fetch("ACCESS_TOKEN")}"
+    req.body = payload.to_json
   end
+  puts res.inspect
 else
   puts "No images were compressed"
 end
